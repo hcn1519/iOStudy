8f9e92d3969aa0ac168a55f9bd01aec083717e14
HannaJeon <jks7psj@gmail.com>
2017-09-07 02:15:52 +0900

    AutoLayout 적용

---
+++ Hanna/week1/LoginPage/LoginPage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by HannaJeon on 2017. 9. 7..
+//  Copyright © 2017년 HannaJeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
