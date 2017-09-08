1c1b2db54d674c9a2f40104fe8aaa59d0b8bc73b
lmjing <lmjing1234@gmail.com>
2017-09-08 16:14:28 +0900

    LoginView Project 생성

---
+++ Mijeong/week1/LoginView/LoginView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginView
+//
+//  Created by 이미정 on 2017. 9. 8..
+//  Copyright © 2017년 이미정. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
