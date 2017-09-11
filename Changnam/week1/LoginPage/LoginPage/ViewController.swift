25db9c77affe78869c9d9c19c7e35d4619d6d2e1
Hong, Chang Nam <hcn1519@gmail.com>
2017-09-12 01:31:12 +0900

    LandScape 오토레이아웃 수정, LandScape animation 적용

--- Changnam/week1/LoginPage/LoginPage/ViewController.swift
+++ Changnam/week1/LoginPage/LoginPage/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldCenterYConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var pwdTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        pwdTextField.delegate = self
+        idTextField.delegate = self
     }
-
+    
+    func isLandScape() -> Bool {
+        return (UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight)
+    }
 
 }
 
+// MARK: TextField 애니메이션, focus 이동
+extension ViewController: UITextFieldDelegate {
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if isLandScape() {
+            switch textField {
+            case idTextField:
+
+                self.textFieldCenterYConstraint.constant = -40
+
+                UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 5,
+                               options: .curveEaseInOut, animations: {
+                                self.view.layoutIfNeeded()
+                }, completion: nil)
+            default:
+                break
+            }
+        }
+        return true
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if isLandScape() {
+            switch textField {
+            case pwdTextField:
+
+                self.textFieldCenterYConstraint.constant = 0
+                textField.resignFirstResponder()
+
+                UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 5,
+                               options: .curveEaseInOut, animations: {
+                                self.view.layoutIfNeeded()
+                }, completion: nil)
+
+            default:
+                textField.resignFirstResponder()
+                pwdTextField.becomeFirstResponder()
+            }
+
+        }
+        return true
+    }
+
+}
