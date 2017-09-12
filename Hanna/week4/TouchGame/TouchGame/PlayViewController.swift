92118285b0a4955f51b67c5841ef29fd55fe82ab
HannaJeon <jks7psj@gmail.com>
2017-09-12 13:31:47 +0900

    숫자 버튼 생성

--- Hanna/week4/TouchGame/TouchGame/PlayViewController.swift
+++ Hanna/week4/TouchGame/TouchGame/PlayViewController.swift
@@ -12,13 +12,32 @@ class PlayViewController: UIViewController {
     
     @IBOutlet weak var homeButton: UIButton!
     @IBOutlet weak var historyButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
     
+    var buttons = [UIButton]()
+    var playing: Bool = false {
+        didSet {
+            switch playing {
+            case true:
+                startButton.isHidden = true
+                historyButton.isEnabled = false
+                historyButton.alpha = 0.6
+            case false:
+                startButton.isHidden = false
+                historyButton.isEnabled = true
+                historyButton.alpha = 1
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        makeButtons()
         homeButton.addTarget(self, action: #selector(homeButtonAction), for: .touchUpInside)
         historyButton.addTarget(self, action: #selector(historyButtonAction), for: .touchUpInside)
+        startButton.addTarget(self, action: #selector(startButtonAction(_:)), for: .touchDown)
     }
     
     func homeButtonAction() {
@@ -31,15 +50,60 @@ class PlayViewController: UIViewController {
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func startButtonAction(_ sender: UIButton) {
+        playing = !playing
+        mixButtons()
     }
-    */
-
+    
 }
+
+extension PlayViewController {
+    func makeButtons() {
+        let buttonSize = Double(containerView.frame.width / sqrt(25))
+        var buttonX = 0.0
+        var buttonY = 0.0
+        for i in 1...25 {
+            let button = UIButton(frame: CGRect(x: buttonX, y: buttonY, width: buttonSize, height: buttonSize))
+            button.tag = i
+            button.setTitle("\(i)", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+            button.layer.borderWidth = 4
+            button.layer.borderColor = UIColor.white.cgColor
+            
+            containerView.addSubview(button)
+            buttons.append(button)
+            
+            buttonX += buttonSize
+            if buttonX >= Double(containerView.frame.width) {
+                buttonX = 0.0
+                buttonY += buttonSize
+            }
+        }
+    }
+    
+    func mixButtons() {
+        var randomNumbers = [Int]()
+
+        for i in 1...buttons.count {
+            randomNumbers.insert(i, at: Int(arc4random_uniform(UInt32(i))))
+        }
+        
+        for button in buttons {
+            button.tag = randomNumbers.removeFirst()
+            button.setTitle("\(button.tag)", for: .normal)
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
