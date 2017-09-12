bcba74f621caa9993ecc8945bd2d8b8b56801bb5
lmjing <lmjing1234@gmail.com>
2017-09-13 00:27:14 +0900

    디바이스 별 키보드 올라올 경우 화면 이동 구현

--- Mijeong/week1/LoginView/LoginView/ViewController.swift
+++ Mijeong/week1/LoginView/LoginView/ViewController.swift
@@ -12,15 +12,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var IDTextField: UITextField!
     @IBOutlet weak var PasswordTextField: UITextField!
-
+    @IBOutlet weak var containerStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardwillShow), name: .UIKeyboardWillShow, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func keyboardwillShow(notification: Notification) {
+        let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue
+        if let keyboardHeight = keyboardFrame?.cgRectValue.height,
+            containerStackView.frame.maxY > self.view.frame.height - keyboardHeight{
+            let newBoundY = keyboardHeight - (self.view.frame.height - containerStackView.frame.maxY)
+            self.view.bounds = CGRect(origin: CGPoint(x: self.view.bounds.origin.x, y: newBoundY) , size: self.view.bounds.size)
+        }
     }
 
     @IBAction func SignUpButtonAction(_ sender: Any) {
