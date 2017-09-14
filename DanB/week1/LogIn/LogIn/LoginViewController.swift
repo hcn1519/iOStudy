3ab94bf4cb2adebc5993fe5a45ff4ead048d2356
Danbee Lee <miliam5234@gmail.com>
2017-09-14 19:22:05 +0900

    hide keyboard

--- DanB/week1/LogIn/LogIn/LoginViewController.swift
+++ DanB/week1/LogIn/LogIn/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -16,12 +16,22 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        idTextField.delegate = self
+        passwordTextField.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //키보드숨기기
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
     @IBAction func touchedSignIn(_ sender: Any) {
