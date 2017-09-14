879dceed9434fa7cef8ff0ba2835e2aa7eece489
Danbee Lee <miliam5234@gmail.com>
2017-09-14 18:00:01 +0900

    week1 start

---
+++ DanB/week1/LogIn/LogIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LogIn
+//
+//  Created by Danb on 2017. 9. 14..
+//  Copyright © 2017년 Danb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
